98c33c6ba8bf4be011e4737e7bf4dcccee37b798
ChimeraBlack1 <chimerablaque@hotmail.com>
2017-11-08 20:28:50 -0500

    init commit

---
+++ src/components/Invoice.js
@@ -0,0 +1,31 @@
+
+
+// RELATED FIELDS	
+	// ID
+	// Number
+	// Quote#
+	// Sales Order#
+	// PO#
+	// Customer
+	// Bill to Addy
+	// Ship to Addy
+	// My Addy
+	// My HST Reg #
+	// Items
+	// Subtotal
+	// HST (tax) Collected
+	// Balance
+	// Payments
+	// Open Balance
+	// Total
+	// Cost
+	// Profit
+	// Transaction Created By:
+	// Client Notes
+
+
+// Prepopulate fields with relevant data
+
+// Validate input and Save data to database
+
+// POPOUT this window
---
+++ src/components/menu.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+const styles = {
+
+    SideNav: {
+      height: "100vh",
+      width: "20%",
+      backgroundColor: "#34495e",
+      margin: 0,
+      padding: 0,
+    },
+
+    item: {
+      color: "#000",
+      fontSize: 40,
+      fontWeight: "bold",
+      
+      cursor: "pointer",
+
+      paddingLeft: "12%",
+      paddingTop: "8%",
+
+    } 
+
+  };
+
+export default class Menu extends Component {
+
+  render() {
+    return (
+      <div style={styles.SideNav}>
+        <div style={styles.item}>
+            Customers
+        </div>
+        <div style={styles.item}>
+            Vendors
+        </div>
+        <div style={styles.item}>
+            Inventory
+        </div>
+        <div style={styles.item}>
+            General
+        </div>
+        <div style={styles.item}>
+            Accounting
+        </div>
+        <div style={styles.item}>
+            Reports
+        </div>
+      </div>
+    );
+  }
+}
+
+
